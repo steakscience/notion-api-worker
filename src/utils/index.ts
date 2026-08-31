f55022361ab3dca346bbdc3c36d2b6abf440975f
Nirawit Jittipairoj <sixfingeredamish@gmail.com>
2026-08-31 21:41:20 +0300

    maybe fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { HandlerRequest } from "../notion-api/types.js";
 
 export const getNotionToken = (c: HandlerRequest) => {
   return (
-    c.env?.NOTION_TOKEN ||
+    process.env.NOTION_TOKEN ||
     (c.req.header("Authorization") || "").split("Bearer ")[1] ||
     undefined
   );
